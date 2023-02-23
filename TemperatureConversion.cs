8efac0fb8310e4498951eec3b34084873a2bdedf
saidatla201 <saidatla201@gmail.com>
2023-02-23 20:43:15 +0530

    Adding Temperature Conversion Program

---
+++ TemperatureConversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public class TemperatureConversion
+    {
+        public static void GetTempConvert()
+        {
+            // Celsius to Fahrenheit
+            Console.WriteLine("Enter a temprature in Celsius  : ");
+            float valInCelsius = Convert.ToInt32(Console.ReadLine());
+            float celToFahren = ((valInCelsius * 9 / 5) + 32);
+            Console.WriteLine("temp in fahrenheit :" + celToFahren);
+            // Fahrenheit to Celsius
+            Console.WriteLine("Enter a temprature in Fahrenheit : ");
+            float valInFahren = Convert.ToSingle(Console.ReadLine());
+            float fahToCelsius = (valInFahren - 32) * (5 / (float)9);
+            Console.WriteLine("temp in Celsius :" + fahToCelsius);
+
+        }
+    }
+}
