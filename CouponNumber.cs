cb3761af3600f88168c439d4b39c60f06cdb528c
saidatla201 <saidatla201@gmail.com>
2023-02-23 20:46:52 +0530

    Adding Coupon Number program

---
+++ CouponNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public  class CouponNumber
+    {
+        public static void GetCouponNumbers()
+        {
+            int coupon;
+            Console.WriteLine("Enter a Number of Coupons Required : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                coupon = GetRandomNum();
+                Console.WriteLine(coupon);
+            }
+        }
+        public static int GetRandomNum()
+        {
+            Random random = new Random();
+            random.Next();
+            return random.Next();
+        }
+    }
+}
