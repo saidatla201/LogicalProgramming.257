fe1edc6a923e21e75aa55d72601a969a22f1c881
saidatla201 <saidatla201@gmail.com>
2023-02-23 20:40:14 +0530

    Adding PrimeNumber Program

---
+++ Primenumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public  class Primenumber
+    {
+        public static void PrimeNum()
+        {
+            int Prime = 1;
+            Console.WriteLine("Enter the number ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i=2;i<=number/2;i++)
+            {
+                if(number %i==0)
+                {
+                    Prime = 0;
+                }
+            }
+            if(Prime ==1)
+            {
+                Console.WriteLine("It's a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("It's not a Prime Num");
+            }
+        }
+    }
+}
